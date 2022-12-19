3adf47b73ef6087a14191c2e5f5a9963a347effb
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-12-19 12:19:58 -0500

    Set up contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,37 @@
+import axios from 'axios'
+import { useState } from 'react'
+
 const Contact = () => {
+    const initialState = {
+        name: "",
+        email: "",
+        message: ""
+    }
+
+    const [formState, setFormState] = useState(initialState)
+
+    const handleChange = (e) => {
+        setFormState({...formState, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        // await axios.get('https://script.google.com/macros/s/AKfycbxAqwIggGlCKp2QSa-BfG1JYESCOP0vCinFx5BAdhD1mQ-isfAlagfcpCkZ2aA91CJG-w/exec', formState)
+        setFormState(initialState)
+    }
+    
     return (
         <section id="contact">
             <h1>Contact</h1>
-            {/* <form action={`mailto: ${process.env.REACT_APP_EMAIL}`} method="POST">
-            <label for='name' >Name:</label>
-            <input type='text' name='name' />
-            <label for='email' >Email:</label>
-            <input type='text' name='email' />
-            <label for='message' >Message:</label>
-            <textarea row="6" cols="20" name='message' />
-            <button type='submit' >Submit</button>
-            </form> */}
+            <form onSubmit={handleSubmit} >
+                <label htmlFor='name' >Name:</label>
+                <input type='text' name='name' value={formState.name} onChange={handleChange} />
+                <label htmlFor='email' >Email:</label>
+                <input type='text' name='email' value={formState.email} onChange={handleChange} />
+                <label htmlFor='message' >Message:</label>
+                <textarea row="6" cols="20" name='message' value={formState.message} onChange={handleChange} />
+                <button type='submit' >Submit</button>
+            </form>
         </section>
     )
 }
