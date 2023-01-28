77b2e0b0ea29cfbff4ee4210234b5c881a0a3c19
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-01-28 16:13:36 -0500

    Styled project cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,8 +3,8 @@ import '../styles/ProjectCard.css'
 const ProjectCard = ({name, image, icons, description, gitHubRepo, deployedSite}) => {
     return (
         <div className='project-card'>
+            <h2>{name}</h2>
             <img className='project-picture' src={image} alt={name}/>
-            <h3>{name}</h3>
             <p>{description}</p>
             <div className='project-links'>
                 <a href={gitHubRepo} target="_blank" rel='noreferrer'>GitHub Repo</a>
@@ -12,7 +12,9 @@ const ProjectCard = ({name, image, icons, description, gitHubRepo, deployedSite}
             </div>
             <div className='project-card-icons'>    
                 {icons?.map((icon, index) => (
-                    <img className="icon" key={index} src={icon} alt="Coding icon"/>
+                    <div key={index}>
+                        {icon}
+                    </div>
                 ))}
             </div>
         </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,23 +4,20 @@ import snakeImage from "../assets/project-images/snake-game.png"
 import openSourcePhoto from "../assets/project-images/open-source-photo-aggregator.png"
 import buddyUp from '../assets/project-images/buddy-up.png'
 import themePark from '../assets/project-images/theme-park.png'
-import css from '../assets/icons/css3.svg'
-import express from '../assets/icons/express.svg'
-import html from '../assets/icons/html5.svg'
-import js from '../assets/icons/js.svg'
-import mongodb from '../assets/icons/mongodb.svg'
-import nodejs from '../assets/icons/node-js.svg'
-import react from '../assets/icons/react.svg'
+import { DiCss3Full, DiHtml5, DiMongodb, DiNodejsSmall, DiReact } from "react-icons/di";
+import { SiExpress, SiJavascript, SiPostgresql, SiSequelize } from 'react-icons/si';
+import { FaVuejs } from 'react-icons/fa'
 
 const Projects = () => {
     return (
         <section id="projects">
             <h1>Projects</h1>
             <div className='project-card-container'>
-                <ProjectCard name="Buddy Up" image={buddyUp} description="BuddyUp allows users to be able to scroll through different user profiles and match with users that are interested in attending the same event." gitHubRepo="https://github.com/kalenluciano/buddy-up-frontend" deployedSite="https://buddy-match-up.herokuapp.com/" icons={[html, css, react, express, nodejs, mongodb, js]}/>
-                <ProjectCard name="Ride or Die: Themepark Edition" image={themePark} description="Utilized a MERN stack (MongoDB, Express.js, React.js, Node.js) to develop a full-stack application guide for rides at various theme parks with create, read, and delete operations during a 3-day hackathon event." gitHubRepo="https://github.com/kalenluciano/ride-or-die-theme-park-edition" deployedSite="https://ride-or-die-theme-park-edition.herokuapp.com/" icons={[html, css, react, express, nodejs, mongodb, js]}/>
-                <ProjectCard name="Open Source Photo Aggregator" image={openSourcePhoto} description="Created a front-end application using React.js to organize user-generated photos by location and time to enable journalists to construct timelines of news events. " gitHubRepo="https://github.com/kalenluciano/open-source-photo-aggregator" deployedSite="https://open-source-photo-aggregator.herokuapp.com/" icons={[html, css, react, express, nodejs, mongodb, js]}/>
-                <ProjectCard name="Snake Game" image={snakeImage} description="Utilized HTML, CSS, and vanilla JavaScript to create a modern take on the classic game of Snake in a web browser" gitHubRepo="https://github.com/kalenluciano/snake" deployedSite="https://snake-video-game.surge.sh/" icons={[html, css, js]} />
+                <ProjectCard name="BeesKnees" image={buddyUp} description="Utilized Vue.js to create a social media frontend that allows users to login, post content, follow other users, and thoughtfully engage with other users’ posts through reactions, comments, and reposts" gitHubRepo="https://github.com/kalenluciano/bees-knees-frontend" deployedSite="http://bees-knees-frontend.herokuapp.com/login" icons={[<DiHtml5 className='icon'/>, < DiCss3Full className='icon'/>, <SiJavascript className='icon'/>, <FaVuejs className='icon'/>, <SiExpress className='icon'/>, <DiNodejsSmall className='icon'/>, <SiPostgresql className='icon'/>, <SiSequelize className='icon'/>]}/>
+                <ProjectCard name="Buddy Up" image={buddyUp} description="BuddyUp allows users to be able to scroll through different user profiles and match with users that are interested in attending the same event." gitHubRepo="https://github.com/kalenluciano/buddy-up-frontend" deployedSite="https://buddy-match-up.herokuapp.com/" icons={[<DiHtml5 className='icon'/>, < DiCss3Full className='icon'/>, <SiJavascript className='icon'/>, <DiReact className='icon'/>, <SiExpress className='icon'/>, <DiNodejsSmall className='icon'/>, <SiPostgresql className='icon'/>, <SiSequelize className='icon'/>]}/>
+                <ProjectCard name="Ride or Die: Themepark Edition" image={themePark} description="Utilized a MERN stack (MongoDB, Express.js, React.js, Node.js) to develop a full-stack application guide for rides at various theme parks with create, read, and delete operations during a 3-day hackathon event." gitHubRepo="https://github.com/kalenluciano/ride-or-die-theme-park-edition" deployedSite="https://ride-or-die-theme-park-edition.herokuapp.com/" icons={[<DiHtml5 className='icon'/>, <DiCss3Full className='icon'/>, <SiJavascript className='icon'/>, <DiReact className='icon'/>, <SiExpress className='icon'/>, <DiNodejsSmall className='icon'/>, <DiMongodb className='icon'/>]}/>
+                <ProjectCard name="Open Source Photo Aggregator" image={openSourcePhoto} description="Created a front-end application using React.js to organize user-generated photos by location and time to enable journalists to construct timelines of news events. " gitHubRepo="https://github.com/kalenluciano/open-source-photo-aggregator" deployedSite="https://open-source-photo-aggregator.herokuapp.com/" icons={[<DiHtml5 className='icon'/>, <DiCss3Full className='icon'/>, <SiJavascript className='icon'/>, <DiReact className='icon'/>, <SiExpress className='icon'/>, <DiNodejsSmall className='icon'/>, <DiMongodb className='icon'/>]}/>
+                <ProjectCard name="Snake Game" image={snakeImage} description="Utilized HTML, CSS, and vanilla JavaScript to create a modern take on the classic game of Snake in a web browser" gitHubRepo="https://github.com/kalenluciano/snake" deployedSite="https://snake-video-game.surge.sh/" icons={[<DiHtml5 className='icon'/>, <DiCss3Full className='icon'/>, <SiJavascript className='icon'/>]} />
             </div>
         </section>
     )
