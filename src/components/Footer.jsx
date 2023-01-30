4b1751bdb11fddfc4ce6d614b43d5094d17395b8
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-01-29 21:05:53 -0500

    Styled footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,13 @@
-import email from '../assets/icons/email.svg'
-import github from '../assets/icons/github.svg'
-import linkedin from '../assets/icons/linkedin.svg'
 import '../styles/Footer.css'
+import { GoMarkGithub } from 'react-icons/go'
+import { AiFillLinkedin, AiOutlineMail } from 'react-icons/ai'
 
 const Footer = () => {
     return (
         <section id="footer">
-            <a href='https://github.com/kalenluciano' target='_blank' rel='noreferrer'><img className='footer-icon' src={github} alt='GitHub logo' /></a>
-            <a href='https://www.linkedin.com/in/kalenluciano/' target='_blank' rel='noreferrer'><img className='footer-icon' src={linkedin} alt='LinkedIn logo' /></a>
-            <a href={`mailto: ${process.env.REACT_APP_EMAIL}`} target='_blank' rel='noreferrer'><img className='footer-icon' src={email} alt='Email logo' /></a>
+            <a href='https://github.com/kalenluciano' target='_blank' rel='noreferrer'><GoMarkGithub className='footer-icon' /></a>
+            <a href='https://www.linkedin.com/in/kalenluciano/' target='_blank' rel='noreferrer'><AiFillLinkedin className='footer-icon' /></a>
+            <a href={`mailto: ${process.env.REACT_APP_EMAIL}`} target='_blank' rel='noreferrer'><AiOutlineMail className='footer-icon' /></a>
         </section>
     )
 }
