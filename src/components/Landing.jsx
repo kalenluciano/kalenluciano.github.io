af5215c404880c402b554ba0ca8c1a45adbaede2
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-01-30 16:38:06 -0500

    Made landing page responsive for desktop

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -7,21 +7,25 @@ import { Icon } from '@iconify/react';
 const Landing = () => {
   return (
     <section id="landing">
-      <h1>Kalen Luciano</h1>
-      <h3>Full-Stack Software Engineer</h3>
-      <div className='landing-links'>
+      <h1 className='top-left'>Kalen Luciano</h1>
+      <div className='center-left'>
+        <h3>Full-Stack Software Engineer</h3>
+      </div>
+      <div className='landing-links bottom-left'>
         <a href="https://docs.google.com/document/d/1f6Ub_9X9cn2RjRJ7zDzhEMlQGk8dkBU43G85jGgb8_4/edit?usp=sharing" target="_blank" rel='noreferrer'>View Resume</a>
         <a href="#contact">Let's Chat</a>
       </div>
-      <img className='profile-pic' src={profilePicture} alt='Profile of Kalen Luciano'/>
-      <section className='landing-icons'>
+      <div className='top-right'>
+        <img className='profile-pic' src={profilePicture} alt='Profile of Kalen Luciano'/>
+      </div>
+      <section className='landing-icons bottom-right'>
         <a href='https://github.com/kalenluciano' target='_blank' rel='noreferrer'><GoMarkGithub className='landing-icon'/></a>
         <a href='https://www.linkedin.com/in/kalenluciano/' target='_blank' rel='noreferrer'><AiFillLinkedin className='landing-icon'/></a>
         <a href={`mailto: ${process.env.REACT_APP_EMAIL}`} target='_blank' rel='noreferrer'><AiOutlineMail className='landing-icon'/></a>
       </section>
       <div className='landing-arrows'>
-        <Icon icon="material-symbols:arrow-back-ios-new-rounded" color="#ffb703" width="48" height="48" rotate={3} />
-        <Icon icon="material-symbols:arrow-back-ios-new-rounded" color="#ffb703" width="36" height="36" rotate={3} />
+        <Icon className='first-arrow' icon="material-symbols:arrow-back-ios-new-rounded" color="#ffb703" width="48" height="48" rotate={3} />
+        <Icon className='middle-arrow' icon="material-symbols:arrow-back-ios-new-rounded" color="#ffb703" width="36" height="36" rotate={3} />
         <Icon className="last-arrow" icon="material-symbols:arrow-back-ios-new-rounded" color="#ffb703" width="24" height="24" rotate={3} />
       </div>
     </section>
