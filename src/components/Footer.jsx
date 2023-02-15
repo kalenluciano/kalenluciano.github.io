ad501d751fdf8c9ad6fcf3ad115223e5f3c40cbe
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-02-14 21:49:54 -0500

    Added hover effects to all other buttons on the page and added new text to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,14 @@ import { AiFillLinkedin, AiOutlineMail } from 'react-icons/ai'
 const Footer = () => {
     return (
         <section id="footer">
-            <a href='https://github.com/kalenluciano' target='_blank' rel='noreferrer'><GoMarkGithub className='footer-icon' /></a>
-            <a href='https://www.linkedin.com/in/kalenluciano/' target='_blank' rel='noreferrer'><AiFillLinkedin className='footer-icon' /></a>
-            <a href={`mailto: ${process.env.REACT_APP_EMAIL}`} target='_blank' rel='noreferrer'><AiOutlineMail className='footer-icon' /></a>
+            <div className='footer-links'>
+                <a href='https://github.com/kalenluciano' target='_blank' rel='noreferrer'><GoMarkGithub className='footer-icon' /></a>
+                <a href='https://www.linkedin.com/in/kalenluciano/' target='_blank' rel='noreferrer'><AiFillLinkedin className='footer-icon' /></a>
+                <a href={`mailto: ${process.env.REACT_APP_EMAIL}`} target='_blank' rel='noreferrer'><AiOutlineMail className='footer-icon' /></a>
+            </div>
+            <p>Portfolio created with React</p>
+            <p className='footer-portfolio-link'><a href='https://www.linkedin.com/in/kalenluciano/' target='_blank' rel='noreferrer'>Portfolio GitHub Repo</a></p>
+            <p>© Kalen Luciano 2023</p>
         </section>
     )
 }
