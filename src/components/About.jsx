607f02f8358d4d72790abff1a6bc63bd8ab163d9
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-05-02 15:46:37 -0400

    Fixed spacing issue in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,6 +12,7 @@ const About = () => {
             <p>In my freelance work, I’ve gained valuable insight on working with cross-functional teams. As a freelance software engineer for Chippy, I’m a part of a team of 6 programmers and UX designers, developing a full-stack application with Next.js, Express, PostgreSQL, and Node.js and streamlining production with a continuous integration pipeline and unit testing with Jest. In my other freelance project for Walking on a Dream, I’m leading a team of 4 programmers and UX designers to define a project scope and business needs to develop a MERN (MongoDB, Express, React, and Node.js) full-stack, mobile-friendly e-commerce website.</p>
             <br></br>
             <p>Prior to my freelance work, I self-taught coding through Codecademy courses before deep diving into the world of web development. During a rigorous 450 hours of instruction and additional self-guided exploration, I developed several applications using React and Vue for the frontend and MongoDB, PostgreSQL, Express, and Node.js for the backend. Working both independently and in teams, I improved my skills in collaboration, creativity, and complex problem solving in a fast-paced production environment.</p>
+            <br></br>
             <p>With my ability to bridge the gap between technical skills and storytelling, I am eager to create engaging and impactful web applications that connect communities and drive positive social change.</p>
         </section>
     )
