d1cd44a0df4515b30ac4f0c52a5ce2807cd2ad8b
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-11-14 18:49:48 -0500

    Change repo buttons

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,16 +1,18 @@
 import '../styles/ProjectCard.css'
 
-const ProjectCard = ({name, image, icons, description, gitHubRepo, deployedSite}) => {
+const ProjectCard = ({ name, image, icons, description, gitHubRepo, frontendRepo, backendRepo, deployedSite }) => {
     return (
         <div className='project-card'>
             <h2>{name}</h2>
-            <img className='project-picture' src={image} alt={name}/>
+            <img className='project-picture' src={image} alt={name} />
             <p>{description}</p>
             <div className='project-links'>
-                <a href={gitHubRepo} target="_blank" rel='noreferrer'>GitHub Repo</a>
-                <a href={deployedSite} target="_blank" rel='noreferrer'>Deployed Site</a>
+                {gitHubRepo && <a href={gitHubRepo} target="_blank" rel='noreferrer'>GitHub Repo</a>}
+                {frontendRepo && <a href={frontendRepo} target="_blank" rel='noreferrer'>Frontend Repo</a>}
+                {backendRepo && <a href={backendRepo} target="_blank" rel='noreferrer'>Backend Repo</a>}
+                {deployedSite && <a href={deployedSite} target="_blank" rel='noreferrer'>Deployed Site</a>}
             </div>
-            <div className='project-card-icons'>    
+            <div className='project-card-icons'>
                 {icons?.map((icon, index) => (
                     <div key={index}>
                         {icon}
