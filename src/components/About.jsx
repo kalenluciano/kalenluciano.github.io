797c0855b56a2e664b6fc1ce1ce3b4e70679fddf
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2024-04-10 11:47:28 -0700

    Update about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,13 +5,13 @@ const About = () => {
         <section id="about">
             <h1>About Me</h1>
             <br></br>
-            <p>I am a full-stack software engineer with strong problem-solving and critical-thinking skills who thrives in collaborating with cross-functional teams. With a passion for climate and social impact, I work on freelance and open source projects that utilize today's most in-demand technologies.</p>
+            <p>I am a full-stack software engineer with strong problem-solving and critical-thinking skills who thrives in collaborating with cross-functional teams.</p>
             <br></br>
-            <p>As an active Out in Tech member, I collaborate closely with a software engineering mentor to gain professional insight and build a project utilizing Next.js, Express, PostgreSQL, and Node.js to calculate tax credits and deductions for schools converting to clean energy for their buildings and other assets or making them energy efficient. In the social impact space, I’m developing features and fixing bugs for a React open source project called PolicyEngine that computes the effects of public policy on household benefits.</p>
+            <p>With a passion for climate and social impact, I work on the engineering team at Bayou Energy building a suite of API’s for companies to effectively deploy clean energy devices across many utilities. Utilizing Python, Django, and OpenAI technology, I develop scripts to fetch and parse utility bills and interval data to extract essential utility information for customers.</p>
             <br></br>
-            <p>In my freelance work, I’ve gained valuable insight on working with cross-functional teams. As a software engineer for Chippy, I’m a part of a team of 6 programmers and UX designers, developing a full-stack application with Next.js, Express, PostgreSQL, and Node.js. I helped implement unit testing with Jest and an automated CI/CD pipeline with GitHub Actions, ensuring reliable code. In my other freelance project for Walking on a Dream, I’m leading a team of 4 programmers and UX designers to define a project scope and business needs to develop a MERN (MongoDB, Express, React, and Node.js) full-stack, mobile-friendly e-commerce website. Working both independently and in teams, I'm improving my skills in collaboration, creativity, and complex problem-solving in a fast-paced production environment.</p>
+            <p>As an active Out in Tech member, I collaborate closely with a software engineering mentor to gain professional insight and build a project utilizing Next.js, Express, PostgreSQL, and Node.js to calculate tax credits and deductions for schools converting to clean energy for their buildings and other assets or making them energy efficient. </p>
             <br></br>
-            <p>With my ability to bridge the gap between technical skills and storytelling, I am eager to create engaging and impactful web applications that connect communities and drive positive social change.</p>
+            <p>Working both independently and in teams, I'm improving my skills in collaboration, creativity, and complex problem-solving in a fast-paced production environment. With my ability to bridge the gap between technical skills and storytelling, I am eager to create engaging and impactful web applications that connect communities and drive positive social change.</p>
         </section>
     )
 }
